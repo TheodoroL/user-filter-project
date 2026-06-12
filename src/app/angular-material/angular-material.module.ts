06550ba9f760bd63795fe2a7d3298aa5bbda8b67
Lucas <lucastheodoromourofernandes@gmail.com>
2026-06-11 21:46:19 -0400

    feat: Criando o component de detalhes

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
 @NgModule({
-  imports: [MatButtonModule, MatIconModule, MatDividerModule],
-  exports: [MatButtonModule, MatIconModule, MatDividerModule],
+  imports: [MatListModule, MatDividerModule],
+  exports: [MatListModule, MatDividerModule],
 })
 export class AngularMaterialModule {}
