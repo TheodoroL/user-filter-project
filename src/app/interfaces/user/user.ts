8685ff024c9145331631bb863fb0a4475d4686a5
Lucas <lucastheodoromourofernandes@gmail.com>
2026-06-11 21:00:02 -0400

    feat: cria estrutura inicial da aplicação e adiciona lista de usuários mockada

---
+++ src/app/interfaces/user/user.ts
@@ -0,0 +1,15 @@
+import { IEndereco } from './anddress';
+import { IStatus } from './status';
+
+export interface IUser {
+  nome: string;
+  email: string;
+  senha: string;
+  idade: number;
+  endereco: IEndereco;
+  telefone: string;
+  ativo: boolean;
+  funcao: string;
+  dataCadastro: string;
+  status: IStatus;
+}
